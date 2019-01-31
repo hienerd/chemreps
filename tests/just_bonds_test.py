10f801f060d4a833f24cb08f2482a942cf04fd19
Dakota Folmsbee <dfolmsbee@gmail.com>
2019-01-31 12:58:13 -0500

    Universal BagMaker

--- tests/just_bonds_test.py
+++ tests/just_bonds_test.py
@@ -1,19 +1,22 @@
 import numpy as np
-from chemreps.utils.molecule import Molecule
+from chemreps.bagger import BagMaker
 import chemreps.just_bonds as jb
 
 
 def test_bag_maker():
-    bags_true = {'OC': [], 'OH': [], 'CC': [], 'HC': []}
-    bags, bag_sizes = jb.bag_maker('data/sdf/')
-    assert bags == bags_true
+    bags_true = {'CC': 7, 'HC': 10, 'OC': 2, 'OH': 2}
+    bagger = BagMaker('JustBonds', 'data/sdf/')
+    print(bagger.bags)
+    assert bagger.bag_sizes == bags_true
 
 
-def test_bag_of_bonds():
-    jbs_true = np.array([0., 0., 0., 0., 0., 0., 36., 36., 36., 0., 0., 0., 0., 0.,
-                         9.67, 9.67, 9.67, 9.67, 9.67, 9.67, 9.67, 9.67, 9.67, 9.67, 0.], dtype=np.float16)
-    bags, bag_sizes = jb.bag_maker('data/sdf/')
-    jbs = jb.bonds('data/sdf/butane.sdf', bags, bag_sizes)
+def test_just_bonds():
+    jbs_true = np.array([36.  , 36.  , 36.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  9.67,
+        9.67,  9.67,  9.67,  9.67,  9.67,  9.67,  9.67,  9.67,  9.67,
+        0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ], dtype=np.float16)
+    bagger = BagMaker('JustBonds', 'data/sdf/')
+    jbs = jb.bonds('data/sdf/butane.sdf', bagger.bags, bagger.bag_sizes)
+    # print([jbs])
     assert np.allclose(jbs, jbs_true, 1e-4) == True
 
 
